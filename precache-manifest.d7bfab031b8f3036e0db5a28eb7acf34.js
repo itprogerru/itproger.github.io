d03a2d37aad937a6bb99a9dad4d2a0b2ffcd16ac
slava <slava_vendenta@mail.ru>
2019-04-01 22:58:59 +0400

    Updates

---
+++ precache-manifest.d7bfab031b8f3036e0db5a28eb7acf34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e6ddf633b4b059773925",
+    "url": "/static/js/main.b4197033.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e6ddf633b4b059773925",
+    "url": "/static/css/main.933cc666.chunk.css"
+  },
+  {
+    "revision": "cdeaa27380f23b9d5ceef4dac18c56e7",
+    "url": "/index.html"
+  }
+];
